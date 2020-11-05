b4d61cab006cd2f220e67feb8d6ace7ca6bb72b2
ethanny2 <ethanny2@gmail.com>
2020-11-05 16:27:28 -0500

    Added in example for transition using CSSTransition component from react-transition-group lib

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { currentWeather, weeklyForecast } from './api/openWeather';
 import Today from './Today';
 import Weekly from './Weekly';
 import './App.css';
-import { Transition } from 'react-transition-group';
+import { CSSTransition } from 'react-transition-group';
 
 /* Debounce a network request on a search bar using lodash library and the
 _.debounce function
@@ -14,19 +14,20 @@ function App() {
 	const [userText, setUserText] = useState('');
 	const [today, setToday] = useState(null);
 	const [weekly, setWeekly] = useState([]);
+	const [triggerAnim, setTriggerAnim] = useState(false);
 	const duration = 500;
 
-	const defaultStyle = {
-		transition: `opacity ${duration}ms ease-in-out`,
-		opacity: 0
-	};
+	// const defaultStyle = {
+	// 	transition: `opacity ${duration}ms ease-in-out`,
+	// 	opacity: 0
+	// };
 
-	const transitionStyles = {
-		entering: { opacity: 0.0 },
-		entered: { opacity: 1 },
-		exiting: { opacity: 0 },
-		exited: { opacity: 0 }
-	};
+	// const transitionStyles = {
+	// 	entering: { opacity: 0.0 },
+	// 	entered: { opacity: 1 },
+	// 	exiting: { opacity: 0 },
+	// 	exited: { opacity: 0 }
+	// };
 	const debounceCurrentWeather = async (searchTerm) => {
 		console.log('Inside here usertext is ' + searchTerm);
 		const response = await currentWeather.get('', {
@@ -62,6 +63,7 @@ function App() {
 			const weatherArray = response.data.daily;
 			weatherArray.shift();
 			setWeekly(weatherArray);
+			setTriggerAnim(true);
 		};
 		if (today) {
 			networkRequest();
@@ -79,22 +81,23 @@ function App() {
 						placeholder='type in a city name'
 						value={userText}
 						id='search'
-						onChange={(e) => setUserText(e.target.value)}
+						onChange={ e => {
+							setTriggerAnim(false);
+							setUserText(e.target.value)
+						}}
 					/>
 				</div>
 			</form>
-			{today ? <Today today={today} /> : null}
+			{today ? (
+				<CSSTransition in={triggerAnim} timeout={duration} appear classNames="weekly">
+							<Today today={today} />
+				</CSSTransition>
+			) : null}
 
 			{weekly.length ? (
-				<Transition in={true} timeout={500} appear>
-					{(state) => {
-						return (
-							<div style={{ ...defaultStyle, ...transitionStyles[state] }}>
+				<CSSTransition in={triggerAnim} timeout={duration} appear classNames="weekly">
 								<Weekly weekArr={weekly} />
-							</div>
-						);
-					}}
-				</Transition>
+				</CSSTransition>
 			) : null}
 		</main>
 	);
